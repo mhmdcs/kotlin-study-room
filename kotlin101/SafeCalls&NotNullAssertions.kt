2de2c930018c18d46d2608fb5d74bfd200fc7264
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-01-16 01:16:28 +0300

    more backing field examples

---
+++ kotlin101/SafeCalls&NotNullAssertions.kt
@@ -0,0 +1,26 @@
+package kotlin101
+
+//https://stackoverflow.com/a/44248897/9133569
+
+/**To understand ? and !! operators better, say you have this function:*/
+//fun main() {
+//    val name: String = null
+//    println(name)
+//}
+/**This will produce the following error at compile at line 2:
+ Null can not be a value of a non-null type String */
+
+/**Now, you can prevent that by adding a safe-call operator to the String type to make it nullable*/
+//fun main() {
+//    val name: String? = null
+//    println(name)
+//}
+/**This produces a result of: null*/
+
+/**Now, if we want the function to throw an NPE exception when the value of name is null, we can add not-null assertion operator at the end of name: */
+//fun main() {
+//    val name: String? = null
+//    println(name!!)
+//}
+/** This will throw a NullPointerException at runtime. */
+
---
+++ properties/BackingFields&Setters&Getters.kt
@@ -0,0 +1,35 @@
+package properties
+
+import kotlin.math.sqrt
+
+//backing field = field
+//same thing, but we'll call it field for short here
+
+class Square {
+
+    //field is how the property's value is to held in memory, it refers to the sideLength's value 4.0, and color's value "Blue" in this case
+
+                           //field
+    var sideLength: Double = 4.0 // the `=` initializer assigns the field directly
+        get() = field //Kotlin auto-generates this getter implicitly for us, if we explicitly call it like this, the compiler yells at us that it's redundant.
+        set(value) { field = value } //Kotlin auto-generates this setter implicitly for us, if we explicitly call it like this, the compiler yells at us that it's redundant.
+
+                       //field
+    val color: String = "Blue" // the `=` initializer assigns the backing field directly
+        get() = "$field-ish" //the compiler doesn't yell at us anymore because we've modified color's getter, we created a custom getter accessor.
+
+    var area: Double //field will not be generated because 1) we have not initialized this property, 2) we did not declare `field` in neither custom the accessors (declaring it in at least one of them would be enough)
+        get() = sideLength*sideLength
+        set(value){
+            sideLength = sqrt(value)
+        }
+}
+
+fun main(){
+
+    val square = Square()
+
+    val side = square.sideLength // indirectly accessing the field via Kotlin's auto-generated getter accessor
+    square.sideLength = 5.0 // indirectly accessing the field via Kotlin's auto-generated setter accessor
+
+}
