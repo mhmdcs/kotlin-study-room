6bf2484c0579b723d5628bb17e472aee3bd4e0d0
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-10-08 23:59:41 +0300

    update

--- random_misc/DontMisuseLetScopeFunctionNullCheck.kt
+++ random_misc/DontMisuseLetScopeFunctionNullCheck.kt
@@ -1,33 +1,38 @@
 package random_misc
 
+// https://miro.medium.com/max/1400/1*i-Uo4RQDd6tNi2Mj8WSUoA.jpeg
+
 fun main() {
 
-    val myValue: String? = "some text"
+    val myValue: String? = "some string value"
 
-    // let() causes unexpected side-effects if it returns null at the end of the block, because run() will be executed too, even if myValue is NOT null.
+    // 1 - using ?.let {}?: run {}
+    // let() might cause an unexpected subtle bug if it returns null at the end of the code block, causing run() to execute too, even if myValue is NOT null.
     myValue?.let {
-        println("myValue is NOT null")
-        someUnrelatedNullableComputation()
+        println("1 - myValue is NOT null")
+        someNullableComputation()
     } ?: run {
-        println("myValue IS null") // this block of code wil be executed because someUnrelatedNullableComputation() returned null!
+        println("1 - myValue IS null") // this block of code will be executed because someNullableComputation() returned null!
     }
 
+    // 2 - if-else null check
     if(myValue != null) {
-        println("myValue is NOT null")
-        someUnrelatedNullableComputation()
+        println("2 - myValue is NOT null")
+        someNullableComputation()
     } else {
-        println("myValue IS null")
+        println("2 - myValue IS null")
     }
 
-    // instead, use also() in place of let() to replicate the same behavior of the traditional if( a!= null){} else{} null-check
+    // 3 - using ?.also {}?: run {}
+    // instead, use also() in place of let() to replicate the same behavior of the traditional if(myValue != null){} else{} null-check
     myValue?.also {
-        println("myValue is NOT null")
-        someUnrelatedNullableComputation()
+        println("3 - myValue is NOT null")
+        someNullableComputation()
     } ?: run {
-        println("myValue IS null")
+        println("3 - myValue IS null")
     }
 }
 
-fun someUnrelatedNullableComputation(): Int? {
+fun someNullableComputation(): Int? {
     return null
 }
