04a7ec1265e9e5eb69b173f66c5488d11d8c0dbf
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2023-04-16 19:32:40 +0300

    references

--- kotlin101/PassByValuePassByReference.kt
+++ kotlin101/PassByValuePassByReference.kt
@@ -2,19 +2,19 @@ package kotlin101
 
 
 // TL;DR
-// In Kotlin/Java, when you pass an object to a function, you can only mutate its content, and you CANNOT reassign it.
+// In Kotlin/Java, when you pass an object's reference to a function, you can only mutate the object's content, and you CANNOT reassign it.
 
 // https://stackoverflow.com/a/59124310/9133569
 // https://stackoverflow.com/a/70736703/9133569
 fun main() {
 
-    // if an object is passed to a method and that object has public set variables and set methods, then its original state and behavior CAN be altered.
+    // if a reference to an object is passed to a method, and that object has public set variables and set methods, then its original state and behavior CAN be altered.
     val someObject = SomeObject()
     println(someObject.string)
     someMethod(someObject)
     println(someObject.string)
 
-    // if an object is passed to a method and that object doesn't have any public set variables and set methods (for example String, Int, etc these are all immutable objects), then its original state and behavior CANNOT be altered.
+    // if a reference to an object is passed to a method, and that object doesn't have any public set variables and set methods (for example String, Int, etc these are all immutable objects), then its original state and behavior CANNOT be altered.
     val someString = "100"
     println(someString)
     someMethod(someString)
@@ -22,12 +22,12 @@ fun main() {
 }
 
 fun someMethod(someString: String) {
-  //  someString = "300" // compiler error, all function parameters are implicitly final aka val in Kotlin, this would compile in Java but it would do also nothing, since you cannot reassign objects that are passed by method, primitives or not, in the JVM.
+  //  someString = "300" // compiler error, all function parameters are implicitly final aka val in Kotlin, this would compile in Java but it would do also nothing, since you cannot reassign objects that are passed to methods, be it primitives or complex objects, in the JVM.
 }
 
 fun someMethod(someObject: SomeObject) {
     someObject.string = "new something" // you can alter the original object's internal state/behavior if it has public set members
-  // someObject = SomeObject() //  someString = "300" // compiler error, all function parameters are implicitly final aka val in Kotlin, this would compile in Java but it would do also nothing, since you cannot reassign objects that are passed by methods, primitives or not, in theJVM.
+  // someObject = SomeObject() // compiler error, all function parameters are implicitly final aka val in Kotlin, this would compile in Java but it would do also nothing, since you cannot reassign objects that are passed to methods, be it primitives or complex objects, in the JVM.
 }
 
 class SomeObject {
@@ -36,7 +36,7 @@ class SomeObject {
 
 /**
  * In other words, it's impossible to change the initial value of a primitive variable like Int or String by passing it as an argument
- * when calling a method. But it's doable with an object, but ONLY in the case it's a mutable object.
- * For example, you can change the internals of you instance of your SomeObject class Obj, or an instance oof ByteArray
- * or List or Map class, etc with their public setter methods, but you cant change an Int or String, these classes/types are designed to be immutable.
+ * when calling a method. But this is possible with an object, but ONLY in the case it's a mutable object (i.e. it has public set methods).
+ * For example, you can change the internals of your instance of your SomeObject class, or an instance of ByteArray, or AtomicBoolean,
+ * or List, or Map class, etc with their public setter methods, but you can't change an Int or String, these classes/types are designed to be immutable, and thus have no public set methods.
  * **/
