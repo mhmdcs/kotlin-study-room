78f935db61e174ad99536b754cf9f21a6061c63c
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2023-08-06 20:57:14 +0300

    delegated properties

--- delegates/LazyProperties2.kt
+++ delegates/LazyProperties2.kt
@@ -7,7 +7,7 @@ package delegates
 // To implement a lazy initialized property, you have to define a backing field that can be initialized anytime. (You can use either a lateinit var or a nullable type, it depends on your preference.)
 // Then, manually override the property accessor method get() and add your initialization logic. Note that you have to do this to every property you want to lazily initialized. So this is a lot of boilerplate code.
 // With Kotlin’s property delegation and lazy initialization built-in  language support, you can now say goodbye to this boilerplate code:
-class Team {
+class ManuallyLazy {
     private var _hero: Hero? = null
     val manuallyLazyHero: Hero
         get() {
@@ -21,8 +21,8 @@ class Team {
 class Hero(val specialtyOne: String, val specialtyTwo: String)
 
 fun main() {
-    val team = Team() // despite us constructing an instance of ManuallyLazy class, its manuallyLazyHero property will never be initialized until we first access it in the following line.
-    println(team.manuallyLazyHero.specialtyTwo)
+    val manuallyLazy = ManuallyLazy() // despite us constructing an instance of ManuallyLazy class, its manuallyLazyHero property will never be initialized until we first access it in the following line.
+    println(manuallyLazy.manuallyLazyHero.specialtyTwo)
 
     // The manual lazy code from above can now be replaced with a one-liner thanks to Kotlin's `by` keyword and standard library lazy() method. lazyHero property will never be initialized until we first access it in the following line.
     val lazyHero: Hero by lazy { Hero("agility", "melee") }
